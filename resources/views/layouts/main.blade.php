336037f66b5df2b5710806d5cf2039ddf3019c9f
elirubi <elisa.rubin@outlook.it>
2024-01-08 21:26:29 +0100

    first commit

---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>{{$title ?? config('app.name')}}</title>
+
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+    </head>
+    <body>
+        {{-- qui la navbar --}}
+        <main class="container d-flex flex-column min-vh-100">
+            <div class="row mt-3">
+                <div class="col-12">
+                    @if(session()->has('success'))
+                    <div class="alert alert-success" role="alert">
+                        {{session('success')}}
+                    </div>
+                    @endif
+
+                    @if(session()->has('error'))
+                    <div class="alert alert-danger" role="alert">
+                        {{session('error')}}
+                    </div>
+                    @endif
+                </div>
+            </div>
+            {{$slot}}
+        </main>
+        <div class="mt-auto container-fluid">
+            {{-- qui il footer --}}
+        </div>
+    </body>
+</html>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,3 @@
+<x-main>
+    <h1>Ciao amici! Ce la faremo!</h1>
+</x-main>
