5b82b5e2932009c7ffdf0a2d0d77bf018c7c7dc6
SandraMuciaccia <sandramuciaccia@gmail.com>
2024-01-09 23:24:40 +0100

    Task Relazione utente annuncio è 1 a n

--- app/Models/User.php
+++ app/Models/User.php
@@ -12,6 +12,10 @@ class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
 
+    public function ads ()
+    {
+        return $this->hasMany(Ad::class);
+    }
     /**
      * The attributes that are mass assignable.
      *
