709db5e0787dd7fc8d847a07adb07eaadd15ad41
elirubi <elisa.rubin@outlook.it>
2024-01-09 17:47:52 +0100

    sistemata welcome + implementato fortify

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,20 +1,16 @@
 <x-main>
-    <h1>Ciao amici! Ce la faremo!</h1>
-
+  
+  <h1>Ciao amici! Ce la faremo!</h1>
     {{-- Bottone “inserisci annuncio” in home disponibile solo a utenti loggati. --}}
-<form method="POST" action="{{route('InserisciAnnuncio')}}">
-      @csrf
+  @auth
+    <a href="{{ route('InserisciAnnuncio') }}" class="btn btn-outline-success btn-sm">Inserisci annuncio</a>
+    
+  @endauth  
 
-      @auth
-        <button class="btn btn-outline-success btn-sm" type="submit">Inserisci annuncio</button>
-      @endauth  
-
-      @guest
-      <p>Registrati per inserire il tuo primo annuncio!</p>
-      <a href="{{ route('register') }}" class="btn btn-primary">Registrati</a>
-      <a href="{{ route('login') }}" class="btn btn-primary">Accedi</a>
-      @endguest
-
-</form>
+  @guest
+  <p>Registrati per inserire il tuo primo annuncio!</p>
+  <a href="{{ route('register') }}" class="btn btn-primary">Registrati</a>
+  <a href="{{ route('login') }}" class="btn btn-primary">Accedi</a>
+  @endguest
 
 </x-main>
