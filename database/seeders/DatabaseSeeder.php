e19b6cd70eba3e2e6366355beab9bd165108fb53
SandraMuciaccia <sandramuciaccia@gmail.com>
2024-01-09 21:55:30 +0100

    Creazione categories, più relazione tra categoria e annuncio 1 a n

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Category;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,6 +14,24 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+
+        $categories = [
+            'Elettronica e Tecnologia',
+            'Veicoli' ,
+            'Immobili' ,
+            'Lavoro e Occupazione',
+            'Arredamento e Casa',
+            'Abbigliamento e Accessori',
+            'Sport e Tempo Libero',
+            'Animali e Accessori',
+            'Servizi' ,
+            'Collezionismo e Antiquariato'
+        ];
+
+        foreach($categories as $category) {
+            Category::create(['name'=>$category]);
+        }
+        
         // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
