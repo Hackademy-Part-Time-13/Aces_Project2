96c844428898346ef5e9ddcc27c42d549fe31eef
elirubi <elisa.rubin@outlook.it>
2024-01-13 21:44:48 +0100

    aggiunto tooglebutton per show/hidden password

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,27 +1,31 @@
 <x-main title="Login">
-    <h1 class="text-center my-5">Login</h1>
-    <form action={{route('login')}} method="post" class="col-12 col-md-8 col-lg-6 mx-auto">
-      @csrf
-      <div class="form-floating mb-3">
-        <input type="text" name="email" class="form-control @error('email') is-invalid @enderror" id="email" placeholder="name@example.com" value={{old('email')}}>
-        <label for="email">Email address</label>
-        @error('email') 
-          <div class="small text-danger">{{$message}}</div>                
-        @enderror 
-      </div>
-      <div class="form-floating mb-3">
-        <input type="password" name="password" class="form-control @error('password') is-invalid @enderror" id="password" placeholder="Password">
-        <label for="password">Password</label>
-        @error('password') 
-          <div class="small text-danger">{{$message}}</div>                
-        @enderror 
-      </div>
-      <div class="mb-3 form-check">
-        <input type="checkbox" class="form-check-input" id="remember" name="remember">
-        <label class="form-check-label small" for="remember">Remember me</label>
-      </div>
-      <button type="submit" class="btn btn-primary col-12">Submit</button>
-    </form>
+  <h1 class="text-center my-5 pt-lg-5">Login</h1>
+  <form action={{route('login')}} method="post" class="col-12 col-md-8 col-lg-6 mx-auto">
+    @csrf
+    <div class="mb-3">
+      <label for="email" class="form-label mb-0 small ms-2 fst-italic">Email address</label>
+      <input type="text" name="email" class="form-control @error('email') is-invalid @enderror" id="email" placeholder="name@example.com" value={{old('email')}}>
+      @error('email') 
+        <div class="small text-danger">{{$message}}</div>                
+      @enderror 
+    </div>
 
-    {{-- <a class="mt-5 btn btn-outline-success col-12 col-md-8 col-lg-6 mx-auto" href="/auth/google">Login with Google</a> --}}
+    <div class="mb-3">
+      <label for="password" class="form-label mb-0 small ms-2 fst-italic">Password</label>
+      <div class="input-group">
+        <input type="password" name="password" class="form-control @error('password') is-invalid @enderror" id="password" placeholder="Password">
+        <span class="input-group-text eye-psw-button"><i class="fa-regular fa-eye-slash mx-auto" id="tooglepsw-button" role="button"></i></span>
+      </div>      
+      @error('password') 
+        <div class="small text-danger">{{$message}}</div>                
+      @enderror 
+    </div>
+
+    <div class="mb-3 form-check">
+      <input type="checkbox" class="form-check-input" id="remember" name="remember">
+      <label class="form-check-label small" for="remember">Remember me</label>
+    </div>
+    <button type="submit" class="btn btn-primary col-12">Submit</button>
+  </form>
+
 </x-main>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,34 +1,40 @@
 <x-main>
-  <h1 class="text-center my-5">Register</h1>
+  <h1 class="text-center my-5 pt-lg-5">Register</h1>
   <form action={{route('register')}} method="POST" class="col-12 col-md-8 col-lg-6 mx-auto">
     @csrf
-    <div class="form-floating mb-3">
-      <input type="text" name="name" class="form-control @error('name') is-invalid @enderror" id="name" placeholder="name@example.com" value="{{old('name')}}">
-      <label for="name">Full Name</label>
+    <div class="mb-3">
+      <label for="name" class="form-label mb-0 small ms-2 fst-italic">Full Name</label>
+      <input type="text" name="name" class="form-control @error('name') is-invalid @enderror" id="name" placeholder="John Doe" value="{{old('name')}}">
       @error('name') 
         <div class="small text-danger">{{$message}}</div>                
       @enderror 
     </div>
 
-    <div class="form-floating mb-3">
+    <div class="mb-3">
+      <label for="email" class="form-label mb-0 small ms-2 fst-italic">Email address</label>
       <input type="email" name="email" class="form-control @error('email') is-invalid @enderror" id="email" placeholder="name@example.com" value="{{old('email')}}">
-      <label for="email">Email address</label>
       @error('email') 
         <div class="small text-danger">{{$message}}</div>                
       @enderror 
     </div>
 
-    <div class="form-floating mb-3">
-      <input type="password" name="password" class="form-control @error('password') is-invalid @enderror" id="password" placeholder="Password">
-      <label for="password">Password</label>
+    <div class="mb-3">
+      <label for="password" class="form-label mb-0 small ms-2 fst-italic">Password</label>
+      <div class="input-group">
+        <input type="password" name="password" class="form-control @error('password') is-invalid @enderror" id="password" placeholder="Password">
+        <span class="input-group-text eye-psw-button"><i class="fa-regular fa-eye-slash mx-auto" id="tooglepsw-button" role="button"></i></span>
+      </div>
       @error('password') 
         <div class="small text-danger">{{$message}}</div>                
       @enderror 
     </div>
 
-    <div class="form-floating mb-3">
-      <input type="password" name="password_confirmation" class="form-control" id="password" placeholder="Password">
-      <label for="password">Confirm Password</label>      
+    <div class="mb-3">
+      <label for="confirm_password" class="form-label mb-0 small ms-2 fst-italic">Confirm Password</label>
+      <div class="input-group">
+        <input type="password" name="password_confirmation" class="form-control" id="confirm_password" placeholder="Password">
+        <span class="input-group-text eye-psw-button"><i class="fa-regular fa-eye-slash mx-auto" id="toogle-confirm-psw-button" role="button"></i></span>
+      </div>
     </div>
     
     <button type="submit" class="btn btn-primary col-12">Submit</button>
