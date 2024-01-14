e316361c550e05dd6686c757720ee9e07b9add65
Cristiano9318 <cristiano199318@gmail.com>
2024-01-14 17:42:40 +0100

    vista lavora con noi

---
+++ resources/views/revisor/work.blade.php
@@ -0,0 +1,21 @@
+<x-main>
+
+    <div class="container mt-5">
+        <div class="row">
+            <div class="col-12 text-center">
+              <p>Presto</p>
+              <p>Work with us</p>
+              <p>Register and click here</p>
+              <p>Registrati e clicca qui</p>
+              <a href="" class="btn btn-primary">Become revisor</a>
+            </div>
+        </div>
+    </div>
+
+
+
+
+
+
+
+</x-main>
