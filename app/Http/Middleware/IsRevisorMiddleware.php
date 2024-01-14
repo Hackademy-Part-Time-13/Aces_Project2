b1cc27f4d1323c99fc66bdf85b1d734d50b9d095
elirubi <elisa.rubin@outlook.it>
2024-01-14 18:51:37 +0100

    sistemato middleware

--- app/Http/Middleware/IsRevisorMiddleware.php
+++ app/Http/Middleware/IsRevisorMiddleware.php
@@ -6,7 +6,7 @@ use Closure;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
 
-class IsRevisor
+class IsRevisorMiddleware
 {
     /**
      * Handle an incoming request.
