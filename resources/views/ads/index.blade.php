3d4130fafe3d7228cfac383d510714735f52fd67
elirubi <elisa.rubin@outlook.it>
2024-01-17 18:51:26 +0100

    modificato frontend

--- resources/views/ads/index.blade.php
+++ resources/views/ads/index.blade.php
@@ -1,22 +1,22 @@
 <x-main>
        
-    <div class="row my-lg-5">
-      @if(Route::currentRouteName() == 'ads.index')
-      <h3 class="my-4">Our last items</h3>
-      @elseif(Route::currentRouteName() == 'adsByCategory')
-      <h3 class="my-4">{{$categoryName}}</h3>
-      @elseif(Route::currentRouteName() == 'ads.favs')
-      <h3 class="my-4">Your favourites</h3>
-      @elseif(Route::currentRouteName() == 'ads.search')
-      <h3 class="my-4">Items about: {{$query}}</h3>
-      @endif
-      
-      @forelse($ads as $ad)
-      <x-card :ad="$ad"/>
-      @empty
-      <p class="fst-italic">Sorry, no items found here.</p>
-      @endforelse
-      {{ $ads->links() }}
-    </div>
+  <div class="row">
+    @if(Route::currentRouteName() == 'ads.index')
+    <h3 class="my-4">Our last items</h3>
+    @elseif(Route::currentRouteName() == 'adsByCategory')
+    <h3 class="my-4">{{$categoryName}}</h3>
+    @elseif(Route::currentRouteName() == 'ads.favs')
+    <h3 class="my-4">Your favourites</h3>
+    @elseif(Route::currentRouteName() == 'ads.search')
+    <h3 class="my-4">Items about: {{$query}}</h3>
+    @endif
+    
+    @forelse($ads as $ad)
+    <x-card :ad="$ad"/>
+    @empty
+    <p class="fst-italic">Sorry, no items found here.</p>
+    @endforelse
+    {{ $ads->links() }}
+  </div>
    
-  </x-main>
+</x-main>
--- resources/views/revisor/work.blade.php
+++ resources/views/revisor/work.blade.php
@@ -3,11 +3,15 @@
     <div class="container">
         <div class="row">
             <div class="col-6 mt-5">
-                <h1>Un utente ha richiesto di lavorare con noi</h1>
+                <h1>Work with us!</h1>
+                <p>Welcome to our "Join Us" page! We're currently seeking a diligent and detail-oriented Ad Reviewer to join our team. </p>
+                <p>If you have a keen eye for accuracy and a passion for maintaining high-quality content, we'd love to hear from you. Review and ensure the integrity of our site's advertisements, contributing to a seamless user experience. </p>
+                <p>Ready to be a crucial part of our team? Click below. </p>
+                <p>Join us in shaping a reliable and trustworthy online environment.</p>
                 <form action="{{route('work.mail')}}" method="POST">
                     @csrf
                     <input type="text" class="d-none" name="email" value={{auth()->user()->email}}>
-                    <button type="submit">Invia richiesta</button>
+                    <button class="btn btn-primary" type="submit">Invia richiesta</button>
                 </form>
                
             </div>
