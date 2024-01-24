2dc497cb73d89ad8ef76a3aee067639ffe1fa6d8
Cristiano9318 <cristiano199318@gmail.com>
2024-01-24 20:07:11 +0100

    aggiunta migration, work in progress user story 6

--- app/Models/Image.php
+++ app/Models/Image.php
@@ -13,25 +13,25 @@ class Image extends Model
 
     protected $fillable = ['path','ads_id'];
 
-    public function ads (){
+    public function ad (){
         return $this->belongsTo(Ad::class);
     }
 
-    // public static function getUrlByFilePath($filePath, $w = null, $h = null){
-    //     if(!$w && !$h){
-    //         return Storage::url($filePath);
-    //     }
+     public static function getUrlByFilePath($filePath, $w = null, $h = null){
+         if(!$w && !$h){
+             return Storage::url($filePath);
+         }
 
-    //     $path = dirname($filePath);
-    //     $filename = basename($filePath);
-    //     $file = "{$path}/crop_{$w}x{$h}_{$filename}";
+         $path = dirname($filePath);
+         $filename = basename($filePath);
+         $file = "{$path}/crop_{$w}x{$h}_{$filename}";
 
-    //     return Storage::url($file);
+         return Storage::url($file);
 
-    // }
+     }
 
-    // public function getUrl($w = null , $h = null){
-    //     return Image::getUrlByFilePath($this->path, $w, $h);
-    // }
+     public function getUrl($w = null , $h = null){
+         return Image::getUrlByFilePath($this->path, $w, $h);
+     }
 }
     
